9f2c33f5824b1c77e866872b405b3278c6de3928
jsu2970 <jsu2970@gmail.com>
2025-03-31 19:44:36 +0900

    Update quiz_menu.py
    모드 선택 시 이미지 표시

--- src/UI_main/quiz_menu.py
+++ src/UI_main/quiz_menu.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from PIL import Image, ImageTk
+from tkinter import Tk, Frame
 
 # 현재 테마 상태 변수
 current_theme = "flatly"  # 기본 테마 (라이트 모드)
@@ -8,7 +10,7 @@ current_theme = "flatly"  # 기본 테마 (라이트 모드)
 # 메인 윈도우 생성
 root = ttk.Window(themename=current_theme)  # ttkbootstrap 사용
 root.title("영단어 학습 프로그램")
-root.geometry("320x450")
+root.geometry("400x600")  #320 450
 root.resizable(False, False)
 
 # 스타일 설정
@@ -18,6 +20,7 @@ style.configure("Normal.TEntry", foreground="black")
 
 def quiz_menu():
     from quiz_interpret import quiz_interpret
+    from quiz_four_choice import quiz_four_choice
 
     def go_to_menu():
         root.quit()
@@ -37,23 +40,31 @@ def quiz_menu():
 
     # 모드 선택 (OptionMenu)
     mode_var = tk.StringVar(value="Select Mode")
-    modes = ["해석 맞추기", "해석 맞추기", "단어 맞추기", "산성비 게임", "문장 채우기 게임"]
+    modes = ["해석 맞추기", "해석 맞추기", "사지선다형 단어 맞추기", "산성비 게임", "문장 채우기 게임"]
 
-    mode_explain = [
-            "해석 -> 단어 모드:\n사용자가 화면에 나오는 해석을 보고 단어를 입력해서 정답을 맞추는 게임.",
-            "단어 -> 해석 모드:\n사용자가 화면에 나오는 단어를 보고 해석을 입력해서 정답을 맞추는 게임.",
-            "산성비 게임:\n사용자가 화면에서 내려오는 해석을 보고 빠르게 단어를 입력하여 정답을 맞추는 게임.",
-            "문장 채우기게임\n사용자가 화면의 문장을 보고 빈칸에 알맞은 단어를 입력하여 정답을 맞추는 게임."
-        ]
+    #이미지 불러오기
+    image1 = Image.open("z.exampleForGame1.jpg")  # 불러올 이미지 경로
+    image2 = Image.open("z.exampleForGame2.jpg")
+    image3 = Image.open("z.exampleForGame3.jpg")
+    image4 = Image.open("z.exampleForGame4.jpg")
+    image1 = image1.resize((350, 300)) 
+    image2 = image2.resize((350, 300)) 
+    image3 = image3.resize((350, 300)) 
+    image4 = image4.resize((350, 300)) 
+    photo1 = ImageTk.PhotoImage(image1)  #tk에서 사용할 수 있게 변환
+    photo2 = ImageTk.PhotoImage(image2)
+    photo3 = ImageTk.PhotoImage(image3)
+    photo4 = ImageTk.PhotoImage(image4)
+    mode_explain = [photo1, photo2, photo3, photo4]
 
-    # 모드 변경 시 출력
+    # 모드 변경 시 그 모드에 대한 예시를 이미지로 출력
     def handle_mode_change(selected_mode):
         #인덱스 검색
         index = modes.index(selected_mode) - 1 #mode1이 중복되므로 하나 줄여야 함
         if (index < 0):
             index += 1
         
-        label_display1.config(text= mode_explain[index])
+        label_display1.config(image= mode_explain[index])
 
     # OptionMenu 생성 시 command 추가
     mode_menu = ttk.OptionMenu(root, mode_var, *modes, command=handle_mode_change)
@@ -67,23 +78,24 @@ def quiz_menu():
     # 카테고리에 대한 라벨 생성 (초기값)
     label_display1 = ttk.Label(
         frame_display, 
-        text=mode_explain[0],  # 모드에 따른 설명 텍스트
+        image=mode_explain[0],  # 모드에 따른 게임 예시. 초기 설정
         font=("나눔고딕", 16, "bold"),  # 글꼴 설정
         foreground="#3F7D58",
         bootstyle="info",  # 버튼 스타일
         wraplength=250,  # 텍스트가 자동으로 줄바꿈되도록 설정
         justify="center",  # 텍스트 중앙 정렬
         anchor="center",  # 텍스트 중앙 정렬
-    
+        borderwidth=2,  # 테두리 두께
+        relief="raised" #테두리 스타일
     )
     label_display1.pack(pady=20, fill="both", expand=True)
 
     # 모드에 따라 다른 함수 실행
     def mode_1_function():
-        print("Mode 1 실행 중...")
+        quiz_interpret(root)
 
     def mode_2_function():
-        quiz_interpret(root)
+        quiz_four_choice(root)
 
     def mode_3_function():
         print("Mode 3 실행 중...")
@@ -100,7 +112,7 @@ def quiz_menu():
 
         if selected_mode == "해석 맞추기":
             mode_1_function()
-        elif selected_mode == "단어 맞추기":
+        elif selected_mode == "사지선다형 단어 맞추기":
             mode_2_function()
         elif selected_mode == "산성비 게임":
             mode_3_function()
@@ -111,7 +123,9 @@ def quiz_menu():
 
     # 시작 버튼 (맨 아래 배치)
     start_button = ttk.Button(root, text="시작", bootstyle="success", command=start_button_clicked)
-    start_button.pack(pady=20, padx=120, fill="x")
+    start_button.pack(pady=20, padx=150, fill="x")
+    #root.bind("<Return>", lambda event: start_button_clicked()) #엔터키로 동작 가능
+
 
 quiz_menu()
 
